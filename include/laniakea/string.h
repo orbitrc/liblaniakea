1c0d1a71bc810507fabfcf11817a108b4fe0282f
Yujeonja <hardboiled65@gmail.com>
2020-09-04 10:19:14 +0900

    Add extern C.

--- include/laniakea/string.h
+++ include/laniakea/string.h
@@ -3,6 +3,8 @@
 
 #include <laniakea/base.h>
 
+LANIAKEA_EXTERN_C_BEGIN
+
 /**
  * Remove leading/trailing whitespaces and new-lines.
  */
@@ -13,5 +15,7 @@ void laniakea_string_trim(char *str);
  * Compare strings and returns LANIAKEA_TRUE if those equal.
  */
 laniakea_bool laniakea_string_eq(const char *str, const char *other);
+
+LANIAKEA_EXTERN_C_END
 
 #endif /* _LANIAKEA_STRING_H */
